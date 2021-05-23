2b1aeef371f58de90263297ff79c7637fef7ad18
Ilmir <78535469+root0f@users.noreply.github.com>
2021-05-23 12:13:41 +0300

    Переписал компонент App с классового на функциональный. Добавил загрузку из DB.json. Подключил React.memo. Убрал списки в глобальные переменные.

--- src/App.js
+++ src/App.js
@@ -1,74 +1,76 @@
-import React, { useEffect, useState } from "react";
-import { Route } from "react-router-dom";
+import React, { useEffect } from "react";
 import axios from "axios";
-import { connect } from "react-redux";
 
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
+import { Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+
 import  { setPizzas } from "./redux/actions/pizzas";
 
 
-// function App() {
+function App() {
+    const dispatch = useDispatch()
+
+
+    useEffect(()=>{
+        axios
+            .get('http://localhost:3001/pizzas')
+            .then(({ data }) => {
+            dispatch(setPizzas(data))
+            })
+    },[])
+
+  return (
+      <div className="wrapper">
+         <Header />
+          <div className="content">
+            <Route exact path='/' component={Home} />
+            <Route exact path='/cart' component={Cart} />
+          </div>
+      </div>
+  );
+}
+
+export default App;
+
+//Классновый компонент
+
+// class App extends React.Component {
+//     componentDidMount() {
+//      return (
 //
+//              axios.get('http://localhost:3001/db.json').then(({ data }) => {
+//                    this.props.setPizzas(data.pizzas)
+//                  })
 //
-//     useEffect(()=>{
-//         axios
-//             .get('http://localhost:3001/db.json')
-//             .then(({ data }) => {
-//             setPizzas(data.pizzas)
-//             })
-//     },[])
+//      )
+//     }
 //
-//   return (
-//       <div className="wrapper">
-//          <Header />
-//           <div className="content">
-//             <Route exact path='/' render={()=><Home items={pizzas} />} />
-//             <Route exact path='/cart' component={Cart} />
-//           </div>
-//       </div>
-//   );
+//     render() {
+//
+//         return (
+//             <div className="wrapper">
+//                 <Header/>
+//                 <div className="content">
+//                     <Route exact path='/' render={() => <Home items={this.props.items}/>}/>
+//                     <Route exact path='/cart' component={Cart}/>
+//                 </div>
+//             </div>
+//         )
+//     }
 // }
-
-class App extends React.Component {
-
-
-    componentDidMount() {
-     return (
-
-             axios
-                 .get('http://localhost:3001/db.json')
-                 .then(({ data }) => {
-                   this.props.setPizzas(data.pizzas)
-                 })
-
-     )
-    }
-
-    render() {
-        console.log(this.props)
-
-        return (
-            <div className="wrapper">
-                <Header/>
-                <div className="content">
-                    <Route exact path='/' render={() => <Home items={this.props.items}/>}/>
-                    <Route exact path='/cart' component={Cart}/>
-                </div>
-            </div>
-        )
-    }
-}
-
-const mapStateToProps = state => {
-    return {
-        items: state.pizzas.items,
-        filters: state.filters,
-    }
-}
-
-const mapDispatchToProps = {
-    setPizzas,
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+//
+// const mapStateToProps = state => {
+//     return {
+//         items: state.pizzas.items,
+//         filters: state.filters,
+//     }
+// }
+//
+// const mapDispatchToProps = {
+//     setPizzas,
+// }
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,27 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
-function Categories({ items }) {
-    const [activeItem, setActiveItem] = useState(null);
-    const onSelectItem = index => {
-        setActiveItem(index)
+const Categories = React.memo(
+    function Categories({ items, onClickItem }) {
+        const [activeItem, setActiveItem] = useState(null);
+        const onSelectItem = index => {
+            setActiveItem(index)
+            onClickItem(index)
+        }
+
+        return (
+            <div className="categories">
+                <ul>
+                    <li
+                        className={activeItem === null ? 'active' : ''}
+                        onClick={() => setActiveItem(null)}>Все</li>
+                    {items && items.map((item, index) => (
+                        <li
+                            className={activeItem === index ? 'active' : ''}
+                            onClick={() => onSelectItem(index)}
+                            key={`${item}_${index}`}>{item}</li>
+                    ))}
+                </ul>
+            </div>
+        );
     }
 
-    return (
-        <div className="categories">
-            <ul>
-                <li
-                    className={activeItem === null ? 'active' : ''}
-                    onClick={() => setActiveItem(null)}>Все</li>
-                {items && items.map((item, index) => (
-                    <li
-                        className={activeItem === index ? 'active' : ''}
-                        onClick={() => onSelectItem(index)}
-                        key={`${item}_${index}`}>{item}</li>
-                ))}
-            </ul>
-        </div>
-    );
-}
-
+)
 export default Categories;
